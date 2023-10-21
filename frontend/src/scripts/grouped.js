b04a9904558155144638e79fd930551377a06909
Léon van der Kaap <leonkaap@gmail.com>
2023-10-22 01:20:52 +0200

    Added grouping for all categories.

--- frontend/src/scripts/grouped.js
+++ frontend/src/scripts/grouped.js
@@ -1,6 +1,22 @@
 let contents = [];
 function group_by_artist() {
-    fetch('/api/v1/grouped/artist').then(
+    populate_table("/api/v1/grouped/artist", "string");
+}
+
+function group_by_album() {
+    populate_table("/api/v1/grouped/album", "string");
+}
+
+function group_by_year() {
+    populate_table("/api/v1/grouped/year", "integer");
+}
+
+function group_by_genre() {
+    populate_table("/api/v1/grouped/genre", "string");
+}
+
+function populate_table(url, type) {
+    fetch(url).then(
         async (response) => {
             let data = await response.json();;
             contents.length = 0;
@@ -17,9 +33,14 @@ function group_by_artist() {
                 name_td.align = 'left';
                 const link = document.createElement("a");
                 link.addEventListener("click", () => {
-                   alert("Not finished atm!");
+                    alert("Not finished atm!");
                 });
-                link.textContent = group.string;
+                if (type === "string") {
+                    link.textContent = group.string;
+                } else if (type === "integer") {
+                    link.textContent = group.integer;
+                }
+
                 link.href = "#";
                 name_td.appendChild(link);
 
